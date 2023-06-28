ac4bd639eec296eab054374d0f3dc9ff34d47fa6
Jared Gallardo <jared@jareds-mbp.lan>
2023-06-28 10:42:41 -0600

    initial commit

---
+++ demo.js
@@ -0,0 +1,13 @@
+import { OpenAI } from 'langchain/llms/openai'
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+const model = new OpenAI({
+	temperature: 0.9,
+})
+
+const res = await model.call(
+	'What would be a good company name for a company that makes colorful socks?'
+)
+
+console.log(res)
