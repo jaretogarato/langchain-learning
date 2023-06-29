3f5ffdd5d69105e3eacdf8c3e59b3b6693e0279c
Jared Gallardo <jared@jareds-mbp.lan>
2023-06-28 18:02:13 -0600

    create first chain

--- demo.js
+++ demo.js
@@ -1,13 +1,42 @@
-import { OpenAI } from 'langchain'
+// import { OpenAI } from 'langchain'
+import { OpenAI } from 'langchain/llms/openai'
+import { PromptTemplate } from 'langchain/prompts'
+import { LLMChain } from 'langchain'
 import * as dotenv from 'dotenv'
+
 dotenv.config()
+
+const template =
+	'What would be a good company name for a company that makes {product}?'
+
+const promptTemplate = new PromptTemplate({
+	template: template,
+	inputVariables: ['product'],
+})
+
+// const formattedPrompt = await promptTemplate.format({
+// 	product: 'colorful socks',
+// })
+
+// console.log(formattedPrompt)
 
 const model = new OpenAI({
 	temperature: 0.9,
 })
 
-const res = await model.call(
-	'What would be a good company name for a company that makes colorful socks?'
-)
+const chain = new LLMChain({
+	llm: model,
+	prompt: promptTemplate,
+})
+
+const res = await chain.call({
+	product: 'dangerous beige dye',
+})
 
 console.log(res)
+
+// const res = await model.call(
+// 	'What would be a good company name for a company that makes colorful socks?'
+// )
+
+// console.log(res)
