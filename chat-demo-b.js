7271ceb4bcb7232e08192e5028a4d69994bd0046
Jared Gallardo <jared@jareds-mbp.lan>
2023-07-03 20:08:45 -0600

    add short term memory

--- chat-demo-b.js
+++ chat-demo-b.js
@@ -1,28 +1,68 @@
 import { ChatOpenAI } from 'langchain/chat_models/openai'
-import { SerpAPI } from 'langchain/tools'
-import { ChatAgent, AgentExecutor } from 'langchain/agents'
+import {
+	ChatPromptTemplate,
+	SystemMessagePromptTemplate,
+	HumanMessagePromptTemplate,
+	MessagesPlaceholder,
+} from 'langchain/prompts'
+import { ConversationChain } from 'langchain/chains'
+import { BufferMemory } from 'langchain/memory'
+
+// import { SerpAPI } from 'langchain/tools'
+// import { ChatAgent, AgentExecutor } from 'langchain/agents'
 
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-const tools = [
-	new SerpAPI(process.env.SERPAPI_API_KEY, {
-		hl: 'en',
-		gl: 'us',
+const chatPrompt = ChatPromptTemplate.fromPromptMessages([
+	SystemMessagePromptTemplate.fromTemplate(
+		'The following is a friendly conversation between a human and and AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question it truthfully says it does not know.'
+	),
+	new MessagesPlaceholder('history'),
+	HumanMessagePromptTemplate.fromTemplate('{input}'),
+])
+
+const model = new ChatOpenAI({})
+
+const chain = new ConversationChain({
+	memory: new BufferMemory({
+		returnMessages: true,
+		memoryKey: 'history',
 	}),
-]
-
-const model = new ChatOpenAI({
-	temperature: 0,
+	prompt: chatPrompt,
+	llm: model,
 })
 
-const agent = ChatAgent.fromLLMAndTools(model, tools)
-
-const executor = AgentExecutor.fromAgentAndTools({
-	agent: agent,
-	tools: tools,
+const res = await chain.call({
+	input: 'Hello from South Africa',
 })
-
-const res = await executor.run('How many people live in the US in 2023?')
 
 console.log(res)
+
+const res2 = await chain.call({
+	input: 'do you know where I am?',
+})
+
+console.log(res2)
+
+// const tools = [
+// 	new SerpAPI(process.env.SERPAPI_API_KEY, {
+// 		hl: 'en',
+// 		gl: 'us',
+// 	}),
+// ]
+
+// const model = new ChatOpenAI({
+// 	temperature: 0,
+// })
+
+// const agent = ChatAgent.fromLLMAndTools(model, tools)
+
+// const executor = AgentExecutor.fromAgentAndTools({
+// 	agent: agent,
+// 	tools: tools,
+// })
+
+// const res = await executor.run('How many people live in the US in 2023?')
+
+// console.log(res)
